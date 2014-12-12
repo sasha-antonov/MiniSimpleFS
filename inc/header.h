5134ca76f13211501108f03ac307836ed685a4a7
sasha-antonov <sasha.antonov1989@gmail.com>
2014-12-12 14:42:59 +0400

    Revert "MiniSimpleFS"
    This reverts commit 01ab6bfe23c66009fdead214289d240e63573aa2.

--- inc/header.h
+++
@@ -1,9 +0,0 @@
-void format_data(void);
-void output_PC(void);
-void control(void);
-uint16_t find_first_file_in_folder(uint16_t cursor_id_slice);
-uint16_t find_free_slice(void);
-void create_file(char* name_file, uint32_t val_temp);
-void create_folder(char* name_folder, char* name_file, uint32_t val_temp);
-void change_file(char* name_file, uint32_t val_temp);
-void change_folder(char* name_folder);
--- inc/type_temp.h
+++
@@ -1,4 +0,0 @@
-
-typedef unsigned char uint8_t;
-typedef unsigned short int uint16_t;
-typedef unsigned int uint32_t;
